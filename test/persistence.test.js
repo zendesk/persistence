b63b0f0b7137a34c223fc703d7f7ee2d6507d749
Vanchi Koduvayur <vanchi@zendesk.com>
2014-06-17 15:42:15 -0700

    better wording/formatting for persistence.test.js

--- test/persistence.test.js
+++ test/persistence.test.js
@@ -2,89 +2,86 @@ var assert = require('assert'),
     Persistence = require('../persistence.js'),
     client;
 
-exports['given a resource'] = {
+describe('given a connected persistence', function() {
 
-  before: function(done) {
+  before(function(done) {
     Persistence.setConfig({ redis_host: 'localhost', redis_port: 6379 });
     Persistence.connect(function() {
       client = Persistence.redis();
       Persistence.delWildCard('*', done);
     });
-  },
+  });
 
-  after: function(done) {
+  after(function(done) {
     Persistence.delWildCard('*', function() {
       Persistence.disconnect(done);
     });
-  },
+  });
 
-  'non JSON redis string should be filtered out (ie. do not return corrupted data)': function(done) {
+  describe('while handling errors', function() {
+    it('should filter out non JSON redis strings (ie. do not return corrupted data)', function(done) {
 
-    var key = 'persistence.test';
+      var key = 'persistence.test';
 
-    client.del(key, function() {
-      client.hset(key, 'bar1', 'this string should be filtered out', function() {
-        client.hset(key, 'bar2', '"this string should be returned"', function() {
-          Persistence.readHashAll(key, function(result) {
-            assert.deepEqual({ bar2: 'this string should be returned' }, result);
-            done();
-          });
+      client.del(key);
+      client.hset(key, 'bar1', 'this string should be filtered out');
+      client.hset(key, 'bar2', '"this string should be returned"', function() {
+        Persistence.readHashAll(key, function(result) {
+          assert.deepEqual({ bar2: 'this string should be returned' }, result);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('while persisting messages', function() {
+    it('should serialize objects', function(done) {
+      // (nherment) TODO: the result should actually be deserialized because it is being serialized in persistOrdered()
+      // The problem is that radar_client currently deserializes the response.
+      // We need to make the client not deserialize the response so that we can deserialize it here.
+
+      var key = 'persistence.messages.object.test';
+      var objectValue = {
+        foo: 'bar'
+      };
+
+      Persistence.persistOrdered(key, objectValue, function(err) {
+        if(err) {
+          return done(err);
+        }
+        Persistence.readOrderedWithScores(key, undefined, function(replies) {
+
+          assert(replies instanceof Array);
+          assert.equal(2, replies.length);
+          assert.equal('string', typeof replies[0]);
+          assert.equal(JSON.stringify(objectValue), replies[0]);
+          done();
         });
       });
     });
 
-  },
+    it('should serialize strings', function(done) {
+      // (nherment) TODO: the result should actually be deserialized because it is being serialized in persistOrdered()
+      // The problem is that radar_client currently deserializes the response.
+      // We need to make the client not deserialize the response so that we can deserialize it here.
 
-  'persisting messages serializes it when the message is an object': function(done) {
-    // (nherment) TODO: the result should actually be deserialized because it is being serialized in persistOrdered()
-    // The problem is that radar_client currently deserializes the response.
-    // We need to make the client not deserialize the response so that we can deserialize it here.
+      var key = 'persistence.messages.string.test';
+      var stringValue = 'Hello World';
 
+      Persistence.persistOrdered(key, stringValue, function(err) {
+        if(err) {
+          return done(err);
+        }
+        Persistence.readOrderedWithScores(key, undefined, function(replies) {
 
-    var key = 'persistence.messages.object.test';
-    var objectValue = {
-      foo: 'bar'
-    };
-
-    Persistence.persistOrdered(key, objectValue, function(err) {
-      if(err) {
-        return done(err);
-      }
-      Persistence.readOrderedWithScores(key, undefined, function(replies) {
-
-        assert(replies instanceof Array);
-        assert.equal(2, replies.length);
-        assert.equal('string', typeof replies[0]);
-        assert.equal(JSON.stringify(objectValue), replies[0]);
-        done();
+          assert(replies instanceof Array);
+          assert.equal(2, replies.length);
+          assert.equal('string', typeof replies[0]);
+          assert.equal(JSON.stringify(stringValue), replies[0]);
+          done();
+        });
       });
-
     });
-  },
+  });
 
-  'persisting messages serializes it when the message is a string': function(done) {
-    // (nherment) TODO: the result should actually be deserialized because it is being serialized in persistOrdered()
-    // The problem is that radar_client currently deserializes the response.
-    // We need to make the client not deserialize the response so that we can deserialize it here.
-
-
-    var key = 'persistence.messages.string.test';
-    var stringValue = 'Hello World';
-
-    Persistence.persistOrdered(key, stringValue, function(err) {
-      if(err) {
-        return done(err);
-      }
-      Persistence.readOrderedWithScores(key, undefined, function(replies) {
-
-        assert(replies instanceof Array);
-        assert.equal(2, replies.length);
-        assert.equal('string', typeof replies[0]);
-        assert.equal(JSON.stringify(stringValue), replies[0]);
-        done();
-      });
-
-    });
-  }
-
-};
+});
