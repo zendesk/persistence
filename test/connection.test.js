0a02b58ed9de91c8df337964d5afc6aeb017770e
Vanchi Koduvayur <vanchi@zendesk.com>
2014-06-18 13:54:24 -0700

    setup our own redis for connection testing

--- test/connection.test.js
+++ test/connection.test.js
@@ -8,7 +8,7 @@ describe('given a ConnectionHelper', function() {
     connection_settings: {
       redis: {
         host: 'localhost',
-        port: 6379
+        port: 16379
       },
       sentinel: {
         // sentinel master name is required
@@ -31,13 +31,26 @@ describe('given a ConnectionHelper', function() {
   };
 
   describe('with redis configuration', function() {
+      var helper_config = {
+        redis : {
+          ports: [ 16379 ]
+        },
+      };
+    before(function() {
+      process.env.noverbose=true;
+      SentinelHelper.start(helper_config);
+    });
+    after(function() {
+      process.env.noverbose=true;
+      SentinelHelper.stop(helper_config);
+    });
     it('should connect', function(done) {
       var config = JSON.parse(JSON.stringify(configuration));
       config.use_connection = 'redis';
 
       var connection = ConnectionHelper.connection(config);
       connection.establish(function() {
-        assert.deepEqual(connection.config,  { host: 'localhost', port: 6379 });
+        assert.deepEqual(connection.config,  { host: 'localhost', port: 16379 });
         ConnectionHelper.destroyConnection(config, done);
       });
     });
@@ -47,7 +60,7 @@ describe('given a ConnectionHelper', function() {
 
       var connection = ConnectionHelper.connection(config);
       connection.establish(function() {
-        assert.deepEqual(connection.config,  { host: 'localhost', port: 6379 });
+        assert.deepEqual(connection.config,  { host: 'localhost', port: 16379 });
         assert.deepEqual(connection, ConnectionHelper.connection(config));
         ConnectionHelper.destroyConnection(config, done);
       });
@@ -55,7 +68,7 @@ describe('given a ConnectionHelper', function() {
   });
 
   describe('with sentinel configuration', function() {
-      var sentinel_config = {
+      var helper_config = {
         redis : {
           ports: [ 16379, 16380, 16381 ]
         },
@@ -65,11 +78,11 @@ describe('given a ConnectionHelper', function() {
       };
     before(function() {
       process.env.noverbose=true;
-      SentinelHelper.start(sentinel_config);
+      SentinelHelper.start(helper_config);
     });
     after(function() {
       process.env.noverbose=true;
-      SentinelHelper.stop(sentinel_config);
+      SentinelHelper.stop(helper_config);
     });
 
     it('should connect', function(done) {
@@ -110,12 +123,21 @@ describe('given a ConnectionHelper', function() {
     var config = JSON.parse(JSON.stringify(configuration));
     config.use_connection = 'redis';
 
+    var helper_config = {
+      redis : {
+        ports: [ 16379 ]
+      },
+    };
     before(function(done) {
+      process.env.noverbose=true;
+      SentinelHelper.start(helper_config);
       connection = ConnectionHelper.connection(config);
       connection.establish(done);
     });
     after(function(done) {
       ConnectionHelper.destroyConnection(config,done);
+      process.env.noverbose=true;
+      SentinelHelper.stop(helper_config);
     });
     it('should not create different clients for multiple establish calls', function(done) {
       var client1, client2;
